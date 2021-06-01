b0c27a6a76683e07e61b0c832751507061b6eb4d
GM <nicoyazawa0501@gmail.com>
2021-06-01 16:03:37 +0800

    面板初步

--- src/main/java/Car/CarDemo.java
+++ src/main/java/Car/CarDemo.java
@@ -3,6 +3,16 @@ package Car;
 import javax.swing.*;
 
 public class CarDemo {
-    private JPanel panel1;
-    private JButton okButton;
+    private JPanel mianPanel;
+    private JPanel northPanel;
+    private JTextField textField1;
+    private JButton button1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setContentPane(new CarDemo().mianPanel);
+        frame.setSize(300,500);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
