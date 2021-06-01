c6681b71d4d20e8ccc7d02649752a1cf488a9762
GM <nicoyazawa0501@gmail.com>
2021-06-01 15:03:55 +0800

    0601 carwin

---
+++ src/main/java/Car/CarWin.java
@@ -0,0 +1,14 @@
+package Car;
+
+import javax.swing.*;
+
+public class CarWin {
+    static int width = 100;
+    static int height = 500;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(width,height);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
