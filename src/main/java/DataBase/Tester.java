7899773718ef312c54e22e91c34ae8e4f1f46a31
GM <nicoyazawa0501@gmail.com>
2021-06-22 19:44:35 +0800

    0622

--- src/main/java/DataBase/Tester.java
+++ src/main/java/DataBase/Tester.java
@@ -1,4 +1,22 @@
 package DataBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Tester {
+    public static void main(String[] args) {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection conn = DriverManager
+                    .getConnection("jdbc:mariadb://localhost:3306/Student?user=root&password=50527toyota");
+
+//            ResultSet rs = statement.executeQuery();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 }
--- src/main/java/net/URLtest.java
+++ src/main/java/net/URLtest.java
@@ -11,7 +11,8 @@ import java.net.URL;
 public class URLtest {
     public static void main(String[] args) {
         try {
-            URL url = new URL("http://ws.kinmen.gov.tw/001/Upload/0/relfile/0/0/e3717d5f-d538-405a-a077-c90af64ff75a.json");
+            URL url = new URL("http://quality.data.gov.tw/dq_download_json.php?nid=11688&md5_url=9bbac2987cfd35e5ba52de1c1f39a5c3");
+//            http://ws.kinmen.gov.tw/001/Upload/0/relfile/0/0/e3717d5f-d538-405a-a077-c90af64ff75a.json
             HttpURLConnection connection = (HttpURLConnection)url.openConnection();
             InputStream is = connection.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
